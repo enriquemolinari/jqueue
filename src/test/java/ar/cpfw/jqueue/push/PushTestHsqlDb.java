36cfcdd56ec91999220a6fcd2fc9eb0d21c85f5d
Enrique <emolinari@jusrionegro.gov.ar>
2022-09-23 15:25:52 -0300

    adding some tests to the push behaviour

--- src/test/java/ar/cpfw/jqueue/push/PushTestHsqlDb.java
+++ src/test/java/ar/cpfw/jqueue/push/PushTestHsqlDb.java
@@ -1,53 +1,200 @@
 package ar.cpfw.jqueue.push;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Map;
 import org.hsqldb.jdbc.JDBCDataSource;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.jcabi.jdbc.JdbcSession;
 import com.jcabi.jdbc.Outcome;
 
 public class PushTestHsqlDb {
 
-  private Connection getConn() throws SQLException {
-    return DriverManager
-        .getConnection("jdbc:hsqldb:mem:testdb;sql.syntax_pgs=true", "SA", "");
-  }
+  // private Connection getConn() throws SQLException {
+  // return DriverManager
+  // .getConnection("jdbc:hsqldb:mem:testdb;sql.syntax_pgs=true", "SA", "");
+  // }
 
   // TODO: Test que el push esta en una Tx con una conn iniciada...
   // Cambiar a DataSource
   // Ver como mejorar si uso esquema o no...
 
-  @Test
-  public void test() throws SQLException {
-
+  @BeforeEach
+  public void setUp() throws SQLException {
     var ds = new JDBCDataSource();
     ds.setUrl("jdbc:hsqldb:mem:testdb;sql.syntax_pgs=true");
     ds.setUser("SA");
     ds.setPassword("");
 
-    new JdbcSession(ds).sql("CREATE TABLE ar_cpfw_jqueue ( "
-        + "id char(36) NOT NULL,  " + "channel varchar(100) NOT NULL, "
-        + "data text NOT NULL, " + "attempt int, " + "delay int, "
-        + "pushed_at timestamp, " + "CONSTRAINT id_pk PRIMARY KEY (id));")
+    new JdbcSession(ds).sql("DROP SCHEMA PUBLIC CASCADE").execute();
+  }
+
+  @Test
+  public void queuePushIsRollbackIfTxFail() throws SQLException {
+    var ds = new JDBCDataSource();
+    ds.setUrl("jdbc:hsqldb:mem:testdb;sql.syntax_pgs=true");
+    ds.setUser("SA");
+    ds.setPassword("");
+
+    new JdbcSession(ds).autocommit(false)
+        .sql("CREATE TABLE if not exists ar_cpfw_jqueue ( "
+            + "id char(36) NOT NULL,  " + "channel varchar(100) NOT NULL, "
+            + "data text NOT NULL, " + "attempt int, " + "delay int, "
+            + "pushed_at timestamp, " + "CONSTRAINT id_pk PRIMARY KEY (id));")
+        .execute().sql("CREATE TABLE if not exists test (name VARCHAR(50))")
+        .execute().commit();
+
+    var conn = ds.getConnection();
+    try {
+      conn.setAutoCommit(false);
+
+      var queue = JTxQueue.queue(conn);
+      queue.push("Hola Mundo!");
+
+      PreparedStatement st = conn.prepareStatement(
+          "insert into table_does_not_exists (name) values (?)");
+      st.setString(1, "any name");
+      st.executeUpdate();
+      conn.commit();
+
+    } catch (Exception e) {
+      conn.rollback();
+    } finally {
+      conn.setAutoCommit(true);
+      conn.close();
+    }
+
+
+    int totalRows =
+        new JdbcSession(ds).sql("select count(*) from ar_cpfw_jqueue")
+            .select(new Outcome<Integer>() {
+              @Override
+              public Integer handle(final ResultSet rset, final Statement stmt)
+                  throws SQLException {
+                rset.next();
+                return rset.getInt(1);
+              }
+            });
+
+    assertEquals(0, totalRows);
+
+  }
+
+  @Test
+  public void queuePushCanBeDoneInSpecificChannel() throws SQLException {
+    var ds = new JDBCDataSource();
+    ds.setUrl("jdbc:hsqldb:mem:testdb;sql.syntax_pgs=true");
+    ds.setUser("SA");
+    ds.setPassword("");
+
+    new JdbcSession(ds)
+        .sql("CREATE TABLE if not exists ar_cpfw_jqueue ( "
+            + "id char(36) NOT NULL,  " + "channel varchar(100) NOT NULL, "
+            + "data text NOT NULL, " + "attempt int, " + "delay int, "
+            + "pushed_at timestamp, " + "CONSTRAINT id_pk PRIMARY KEY (id));")
         .execute();
 
-    var queue = JTxQueue.queue(ds.getConnection());
-    queue.push("Hola Mundo!");
+    var conn = ds.getConnection();
+    try {
+      var queue = JTxQueue.queue(conn);
+      queue.channel("anotherChannel").push("Hola Mundo!");
 
-    String s = new JdbcSession(ds).sql("select data from ar_cpfw_jqueue")
-        .select(new Outcome<String>() {
+    } finally {
+      conn.close();
+    }
+
+    int totalRows =
+        new JdbcSession(ds).sql("select count(*) from ar_cpfw_jqueue")
+            .select(new Outcome<Integer>() {
+              @Override
+              public Integer handle(final ResultSet rset, final Statement stmt)
+                  throws SQLException {
+                rset.next();
+                return rset.getInt(1);
+              }
+            });
+
+    assertEquals(1, totalRows);
+
+    Map<String, String> outcome = new JdbcSession(ds)
+        .sql("select channel, data, attempt, delay from ar_cpfw_jqueue")
+        .select(new Outcome<Map<String, String>>() {
           @Override
-          public String handle(final ResultSet rset, final Statement stmt)
-              throws SQLException {
+          public Map<String, String> handle(final ResultSet rset,
+              final Statement stmt) throws SQLException {
+
             rset.next();
-            return rset.getString(1);
+            return Map.of("channel", rset.getString(1), "data",
+                rset.getString(2), "attempt", String.valueOf(rset.getInt(3)),
+                "delay", String.valueOf(rset.getInt(4)));
           }
         });
 
-    System.out.println(s);
+    assertEquals("anotherChannel", outcome.get("channel"));
+    assertEquals("Hola Mundo!", outcome.get("data"));
+    assertEquals("0", outcome.get("attempt"));
+    assertEquals("0", outcome.get("delay"));
   }
+
+  @Test
+  public void queuePushCanBeDoneInDefaultChannel() throws SQLException {
+    var ds = new JDBCDataSource();
+    ds.setUrl("jdbc:hsqldb:mem:testdb;sql.syntax_pgs=true");
+    ds.setUser("SA");
+    ds.setPassword("");
+
+    new JdbcSession(ds)
+        .sql("CREATE TABLE if not exists ar_cpfw_jqueue ( "
+            + "id char(36) NOT NULL,  " + "channel varchar(100) NOT NULL, "
+            + "data text NOT NULL, " + "attempt int, " + "delay int, "
+            + "pushed_at timestamp, " + "CONSTRAINT id_pk PRIMARY KEY (id));")
+        .execute();
+
+    var conn = ds.getConnection();
+    try {
+      var queue = JTxQueue.queue(conn);
+      queue.push("Hola Mundo!");
+
+    } finally {
+      conn.close();
+    }
+
+    int totalRows =
+        new JdbcSession(ds).sql("select count(*) from ar_cpfw_jqueue")
+            .select(new Outcome<Integer>() {
+              @Override
+              public Integer handle(final ResultSet rset, final Statement stmt)
+                  throws SQLException {
+                rset.next();
+                return rset.getInt(1);
+              }
+            });
+
+    assertEquals(1, totalRows);
+
+    Map<String, String> outcome = new JdbcSession(ds)
+        .sql("select channel, data, attempt, delay from ar_cpfw_jqueue")
+        .select(new Outcome<Map<String, String>>() {
+          @Override
+          public Map<String, String> handle(final ResultSet rset,
+              final Statement stmt) throws SQLException {
+
+            rset.next();
+            return Map.of("channel", rset.getString(1), "data",
+                rset.getString(2), "attempt", String.valueOf(rset.getInt(3)),
+                "delay", String.valueOf(rset.getInt(4)));
+          }
+        });
+
+    assertEquals("default", outcome.get("channel"));
+    assertEquals("Hola Mundo!", outcome.get("data"));
+    assertEquals("0", outcome.get("attempt"));
+    assertEquals("0", outcome.get("delay"));
+  }
+
+
 }
