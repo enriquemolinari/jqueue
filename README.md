d6de7aec4b3e7a88a5224ff4d11a596939a8d027
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2022-09-24 22:58:57 -0300

    rename workflow, adding badges to the home and another test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # jqueue
+
+![CI](https://github.com/enriquemolinari/jqueue/actions/workflows/tests.yml/badge.svg) [![codecov](https://codecov.io/gh/enriquemolinari/jqueue/branch/main/graph/badge.svg?token=K8NLEOA3WN)](https://codecov.io/gh/enriquemolinari/jqueue)
--- src/test/java/ar/cpfw/jqueue/push/PushHsqlDbTest.java
+++ src/test/java/ar/cpfw/jqueue/push/PushHsqlDbTest.java
@@ -40,7 +40,7 @@ public class PushHsqlDbTest {
   }
 
   @Test
-  public void bla() throws SQLException {
+  public void pushWorksWithTableName() throws SQLException {
     var ds = hSqlDataSource();
     new JdbcSession(ds)
         .sql("CREATE TABLE if not exists schema1jqueuetable ( "
@@ -56,6 +56,57 @@ public class PushHsqlDbTest {
 
     } finally {
       conn.close();
+    }
+
+    int totalRows =
+        new JdbcSession(ds).sql("select count(*) from schema1jqueuetable")
+            .select(new Outcome<Integer>() {
+              @Override
+              public Integer handle(final ResultSet rset, final Statement stmt)
+                  throws SQLException {
+                rset.next();
+                return rset.getInt(1);
+              }
+            });
+
+    assertEquals(1, totalRows);
+
+    Map<String, String> outcome = new JdbcSession(ds)
+        .sql("select channel, data, attempt, delay from schema1jqueuetable")
+        .select(new Outcome<Map<String, String>>() {
+          @Override
+          public Map<String, String> handle(final ResultSet rset,
+              final Statement stmt) throws SQLException {
+
+            rset.next();
+            return Map.of("channel", rset.getString(1), "data",
+                rset.getString(2), "attempt", String.valueOf(rset.getInt(3)),
+                "delay", String.valueOf(rset.getInt(4)));
+          }
+        });
+
+    assertEquals("anotherChannel", outcome.get("channel"));
+    assertEquals("Hola Mundo!", outcome.get("data"));
+    assertEquals("0", outcome.get("attempt"));
+    assertEquals("0", outcome.get("delay"));
+  }
+
+  @Test
+  public void pushWorksWithTableNameAndDataSource() throws SQLException {
+    var ds = hSqlDataSource();
+    new JdbcSession(ds)
+        .sql("CREATE TABLE if not exists schema1jqueuetable ( "
+            + "id char(36) NOT NULL,  " + "channel varchar(100) NOT NULL, "
+            + "data text NOT NULL, " + "attempt int, " + "delay int, "
+            + "pushed_at timestamp, " + "CONSTRAINT id_pk PRIMARY KEY (id));")
+        .execute();
+
+    try {
+      var queue = JTxQueue.queue(ds, "schema1jqueuetable");
+      queue.channel("anotherChannel").push("Hola Mundo!");
+
+    } finally {
+      ds.getConnection().close();
     }
 
     int totalRows =
